b857726e0dba048e9a9f74d3aa7fcc559deb921a
Himank Barve <hbarve1592@gmail.com>
2018-09-29 16:17:10 +0530

    First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+  
+    rangeStart: 0,
+    rangeEnd: Infinity,
+  
+    parser: "flow",
+  
+    requirePragma: true
+    // insertPragma: false
+};
